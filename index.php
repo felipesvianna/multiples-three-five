18e27ff0fd2d96d1906e132bc478703801f26a04
Felipe <felipe.vianna10@gmail.com>
2021-03-25 16:35:37 -0300

    Refactor: organize logic of the code

--- index.php
+++ index.php
@@ -13,11 +13,11 @@ $indexesMultiplesThree = $listGenerator->generateListMultiplesOfThree();
 $indexesMultiplesFive = $listGenerator->generateListMultiplesOfFive();
 $indexesMultiplesThreeAndFive = $listGenerator->generateListMultiplesOfThreeAndFive();
 
-echo '<br><br><b>Multiples of Three And Five</b><br><br>';
-
 $listOperator->inputStringOnIndexesOfAList('ThreeAndFive', $oneToHundredList, $indexesMultiplesThreeAndFive);
 $listOperator->inputStringOnIndexesOfAList('Three', $oneToHundredList, $indexesMultiplesThree);
 $listOperator->inputStringOnIndexesOfAList('Five', $oneToHundredList, $indexesMultiplesFive);
+
+echo '<br><br><b>Multiples of Three And Five</b><br><br>';
 $listOperator->printList($oneToHundredList);
 
 ?>
